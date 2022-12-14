1d0d65024a5a81097b74b23ea66c7b01ee303070
fliblip <wickynjy@gmail.com>
2022-12-14 08:12:19 +0300

    add todos/todoAdded action

--- src/reducer.js
+++ src/reducer.js
@@ -22,8 +22,30 @@ const initialState = {
 // Use the initialState as a default value
 export default function appReducer(state = initialState, action){
     switch(action.type){
-        default:
+        case 'todos/todoAdded': {
+            // We should return a new state object
+            return{
+                // This object holds all the existing state data
+                ...state,
 
+                // However, it includes a new array for the `todos` field
+                todos: [
+                    // with all of the old todos
+                    ...state.todos,
+                    // plus the new todo object
+                    {
+                        // auto-increment ID
+                        id: nextTodoId(state.todos),
+                        text: action.payload,
+                        completed: false
+                    }
+                ]
+            }
+        }
+        default:
+            // When an action type is unrecognizable by our reducer or the reducer doesn't care about that specific action, return the existing state unchanged
             return state
     }
 }
+
+// Next, we'll add the 'todos/todoAdded' action logic
