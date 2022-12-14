90f7b5f64e8add507f6c2a6f9ec264852c8d18a6
fliblip <wickynjy@gmail.com>
2022-12-14 09:04:44 +0300

    combine reducers into the root reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,15 +1,26 @@
+import { combineReducers } from "redux"
+// combineReducers accepts an object where the key names will become the keys in our root state object, and the values are the slice reducer functions that know how to update those slices of the Redux state.
+
 import todosReducer from "./features/todos/todosSlice"
 import filtersReducer from "./features/todos/filtersSlice"
 
-export default function rootReducer(state = {}, action){
-    // always return a new object for the root state
-    return{
-        // the value of `state.todos` is whatever the todos reducer returns
-        todos: todosReducer(state.todos, action),
-        // For both reducers, we only pass in their slice of the state
-        filters: filtersReducer(state.filters, action)
-    }
-}
+const rootReducer = combineReducers({
+    // Define a top-level state field named `todos`, handled by `todosReducer`
+    todos: todosReducer,
+    filters: filtersReducer
+})
+
+/** In this hand-written root reducer, for each slice reducer, the state owned by that reducer is passed in, and the result is assigned back to the root state object, if not for combineReducers, we would have to repeat this patttern */
+
+// export default function rootReducer(state = {}, action){
+//     // always return a new object for the root state
+//     return{
+//         // the value of `state.todos` is whatever the todos reducer returns
+//         todos: todosReducer(state.todos, action),
+//         // For both reducers, we only pass in their slice of the state
+//         filters: filtersReducer(state.filters, action)
+//     }
+// }
 
 
 /** This is the code before we sliced up our reducer
@@ -95,6 +106,5 @@ export default function appReducer(state = initialState, action){
     }
 }
 
-// Next, we'll add the 'todos/todoAdded' action logic
 
 /** */
