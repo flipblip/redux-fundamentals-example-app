8a9abcdbe38ea46e283746536f7ba0365a1a6f05
thrasher <thrasher@viper.moringa>
2022-12-17 04:57:23 -0600

    added remaining todoSlice actions

--- src/features/filters/todosSlice.js
+++ src/features/filters/todosSlice.js
@@ -28,16 +28,40 @@ export default function todosReducer(state = initialState, action){
             ]
         }
         case 'todos/todoToggled': {
-            return state.map(todo =>{
+            return state.map((todo) =>{
                 if (todo.id !== action.payload){
                     return todo
                 }
 
                 return{
                     ...todo,
-                    completed: !todo.completed
+                    completed: !todo.completed,
                 }
             })
+        }
+        case 'todos/colorSelected': {
+            const { color, todoId } = action.payload
+            return state.map((todo) =>{
+                if (todo.id !== todoId){
+                    return todo
+                }
+
+                return{
+                   ...todo,
+                    color,
+                }
+            })
+        }
+        case 'todos/todoDeleted': {
+            return state.filter((todo) => todo.id !== action.payload)
+        }
+        case 'todos/allCompleted': {
+            return state.map((todo) =>{
+                return {...todo, completed: true}
+            })
+        }
+        case 'todos/completedCleared': {
+            return state.filter((todo) => ! todo.completed)
         }
             default:
                     return state
