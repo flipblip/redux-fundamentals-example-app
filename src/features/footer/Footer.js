019a44c0d211ddc4c00e4186bba273c597f93fdb
thrasher <thrasher@viper.moringa>
2022-12-17 04:32:39 -0600

    FilterSlice, colors

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,3 +1,44 @@
+import React from "react"
+
+import {availableColors, capitalize} from '../filters/colors'
+
+
+const colorFilters = ({ value: colors, onChange}) =>{
+    const renderedColors = availableColors.map((color) =>{
+        const checked = colors.includes(color)
+        const handleChange = () =>{
+            const changeType = checked ? 'removed' : 'added'
+            onChange(color, changeType)
+        }
+
+        return(
+            <label key={color}>
+                <input
+                    type="checkbox"
+                    name="color"
+                    checked={checked}
+                    onChange={handleChange}
+                />
+                <span
+                    className="color-block"
+                    style={{
+                        backgroundColor: color,
+                    }}>
+                </span>
+                {capitalize(color)}
+            </label>
+        )
+    })
+
+    return(
+        <div className="filters colorFilters">
+            <h5>Filter by Color</h5>
+            <form className="colorSelection">{renderedColors}</form>
+        </div>
+    )
+}
+
+
 const Footer = () =>{
     const colors = []
     const status = StatusFilters.All
